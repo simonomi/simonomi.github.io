2fb09fccd40641413ddad29102538a8838d3b38d
alice pellerin <thesimonpellerin@gmail.com>
2025-07-17 12:52:47 -0400

    references page

---
+++ references/script.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function() {
+	document.getElementById(location.hash.slice(1)).open = true;
+});
+
+function expandAllDetails() {
+	Array.from(document.getElementsByTagName("details"))
+		.forEach(x => x.open = true)
+}
+
+function collapseAllDetails() {
+	Array.from(document.getElementsByTagName("details"))
+		.forEach(x => x.open = false)
+}
+
